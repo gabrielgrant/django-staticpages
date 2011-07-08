3a917bd833ca619ef206c237a95f569f120c3715
Gabriel Grant <g@briel.ca>
2011-07-08 11:56:53 -0700

    add a setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='django-staticpages',
+    version='0.1.0',
+    packages=['staticpages', 'staticpages.tests'],
+    include_package_data=True,
+    author='Gabriel Grant',
+    author_email='g@briel.ca',
+    license='LGPL',
+    long_description=open('README').read(),
+    url='http://github.org/gabrielgrant/django-staticpages/',
+)
+
