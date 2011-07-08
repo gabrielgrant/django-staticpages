08a4a4d545843b8dd2140d4b8e535cf7461f4a69
Gabriel Grant <g@briel.ca>
2011-07-08 16:32:30 -0700

    add menu activating template tag

---
+++ staticpages/templatetags/staticpages_tags.py
@@ -0,0 +1,13 @@
+# from http://gnuvince.wordpress.com/2007/09/14/a-django-template-tag-for-the-current-active-page/
+from django import template
+register = template.Library()
+
+@register.simple_tag
+def active(request, pattern, regex=False):
+    import re
+    if not regex:
+        pattern = "^%s$" % pattern
+    print pattern, request.path
+    if re.search(pattern, request.path):
+        return 'active'
+    return ''
