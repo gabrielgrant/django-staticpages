c48206474590d240a1a0c7eb6c86630fee9d295f
Gabriel Grant <g@briel.ca>
2012-02-06 01:37:19 -0800

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-staticpages',
-    version='0.1.0',
+    version='0.2.0',
     packages=['staticpages', 'staticpages.tests'],
     include_package_data=True,
     author='Gabriel Grant',
